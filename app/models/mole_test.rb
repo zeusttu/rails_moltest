b2703a81f316d316d50fc3724a760987b06f6248
Zeust the Unoobian <2noob2banoob@gmail.com>
2016-03-19 18:40:26 +0100

    feat(model/test,question): add has_many relationships

--- app/models/mole_test.rb
+++ app/models/mole_test.rb
@@ -1,2 +1,4 @@
 class MoleTest < ActiveRecord::Base
+  has_many :questions
+  accepts_nested_attributes_for :questions
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :mole_test
   belongs_to :answer
+  has_many :answers
+  accepts_nested_attributes_for :answers
 end
